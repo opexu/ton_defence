96ec879a2548b2f0ba08ad9f7ec6226b306afb00
opexu <opexu1986@gmail.com>
2023-01-30 16:55:07 +0300

    +++

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -6,9 +6,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     root: path.resolve(__dirname, 'src'),
-    build: {
-        outDir: '../dist'
-    },
+    // build: {
+    //     outDir: '../dist'
+    // },
     base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
     server: {
         port: 3000,
